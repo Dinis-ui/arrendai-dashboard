11017b69ac3edf46f89a80478c76ab71e07380a2
Dinis sousa <cdborges04@gmail.com>
2026-04-25 15:15:10 +0100

    Adiciona modelo Property

--- backend/properties/models.py
+++ backend/properties/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.conf import settings # Para ligar ao nosso Custom User
 
-# Create your models here.
+class Property(models.Model):
+    # O Senhorio dono desta propriedade (Se o dono for apagado, as casas dele também são)
+    landlord = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='properties')
+    
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    
+    # Localização
+    address = models.CharField(max_length=255)
+    zip_code = models.CharField(max_length=20)
+    city = models.CharField(max_length=100)
+    
+    # Detalhes do imóvel
+    area_sqm = models.DecimalField(max_digits=8, decimal_places=2) # Ex: 85.50 m2
+    monthly_rent = models.DecimalField(max_digits=8, decimal_places=2) # Ex: 1200.00 €
+    typology = models.CharField(max_length=10) # Ex: T0, T1, T2
+    
+    # Estado
+    is_available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.city} ({self.typology})"
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+# Isto diz ao painel para mostrar a nossa tabela de Utilizadores
+admin.site.register(User, UserAdmin)
 
 # Register your models here.
