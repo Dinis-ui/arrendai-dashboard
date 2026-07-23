9ae335d94bd3e8ab2fc9b7e94672a21db884859e
Dinis-ui <cdborges04@gmail.com>
2026-07-23 15:38:03 +0100

    Add Vercel link to CORS allowed origins

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -151,6 +151,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://o-teu-link-da-vercel.vercel.app",
 ]
 
 # Configurações para Upload de Ficheiros (Documentos e Imagens)
